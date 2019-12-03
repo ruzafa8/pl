57a57d833b8fc3a86859fc87ab6848f13efbf9a5
ruzafa8 <ruzafa-@outlook.es>
2019-12-03 19:50:29 +0100

    Adding operators to lexer.l

--- expression.c
+++ expression.c
@@ -1,224 +1,48 @@
 #include "expression.h"
 
-Expression * createExpression(){
-    Expression* e = (Expression *) malloc(sizeof(Expression));
-    e->type = UNKNOWN;
-    return e;
-}
-Expression * createIntegerExpression(int value){
-    Expression* e = (Expression *) malloc(sizeof(Expression));
-    e->value.int_value = value;
-    e->type = INTEGER;
-    e->len = 1;
-    return e;
-}
-Expression * createArrayIntegerExpression(int * values, int len){
-    Expression* e = (Expression *) malloc(sizeof(Expression));
-    e->value.array_int_value = (int *) malloc(sizeof(int) * len);
-    int i;
-    for(i = 0; i < len; i++){
-        (e->value.array_int_value)[i] = values[i];
-    }
-    e->type = ARRAY_INTEGER;
-    e->len = len;
-    return e;
-}
-Expression * createCharacterExpression(char value){
-    Expression* e = (Expression *) malloc(sizeof(Expression));
-    e->value.char_value = value;
-    e->type = CHARACTER;
-    e->len = 1;
-    return e;
-}
-Expression * createArrayCharacterExpression(char * values, int len){
-    Expression* e = (Expression *) malloc(sizeof(Expression));
-    e->value.array_char_value = (char *) malloc(sizeof(char) * len);
-    int i;
-    for(i = 0; i < len; i++){
-        (e->value.array_char_value)[i] = values[i];
-    }
-    e->type = ARRAY_CHARACTER;
-    e->len = len;
-    return e;
-}
-Expression * createDoubleExpression(double value){
-    Expression* e = (Expression *) malloc(sizeof(Expression));
-    e->value.double_value = value;
-    e->type = DOUBLE;
-    e->len = 1;
-    return e;
-}
-Expression * createArrayDoubleExpression(double * values, int len){
-    Expression* e = (Expression *) malloc(sizeof(Expression));
-    e->value.array_double_value = (double *) malloc(sizeof(double) * len);
-    int i;
-    for(i = 0; i < len; i++){
-            (e->value.array_double_value)[i] = values[i];
-    }
-    e->type = ARRAY_DOUBLE;
-    e->len = len;
-    return e;
-}
-Expression * createBooleanExpression(Boolean value){
-    Expression* e = (Expression *) malloc(sizeof(Expression));
-    e->value.boolean_value = value;
-    e->type = BOOLEAN;
-    e->len = 1;
-    return e;
-}
-Expression * createArrayBooleanExpression(Boolean * values, int len){
-    Expression* e = (Expression *) malloc(sizeof(Expression));
-    e->value.array_boolean_value = (Boolean *) malloc(sizeof(Boolean) * len);
-    int i;
-    for(i = 0; i < len; i++){
-        e->value.array_boolean_value[i] = values[i];
-    }
-    e->type = ARRAY_BOOLEAN;
-    e->len = len;
+Expression * createBasicExpression(Typename type){
+    if(type == ARRAY) return NULL;
+    Expression* e = (Expression*) malloc(sizeof(Expression));
+    *(e->type) = (Type*) malloc(sizeof(Type));
+    e->type->name = type;
+    e->type->next = NULL;
     return e;
 }
 
-EXIT_CODE changeIntegerExpression(Expression* e,int value){
-    EXIT_CODE code;
-    if(e->type == UNKNOWN){
-        e->type = INTEGER;
-    }
-
-    if(e->type != INTEGER){
-        code = TYPE_ERROR;
-    } else {
-        e->value.int_value = value;
-        code = SUCCESS;
-    }
-    return code;
+Value * createValue(){
+    return (Value*) malloc(sizeof(Value));
 }
-EXIT_CODE changeArrayIntegerExpression(Expression* e,int value, int position){
-    EXIT_CODE code;
-    if(e->type == UNKNOWN){
-        e->type = ARRAY_INTEGER;
-        e->len = position+1;
-        e->value.array_int_value = (int *) malloc(sizeof(int) * e->len);
-        int i;
-        for(i = 0; i < position; i++){
-            e->value.array_int_value[i] = 0;
-        }
-    }
-
-    if(e->type != ARRAY_INTEGER){
-        code = TYPE_ERROR;
-    } else if(0 > position || position >= e->len) {
-        code = OUT_OF_BOUNDS_ERROR;
-    }else{
-        e->value.array_int_value[position] = value;
-        code = SUCCESS;
-    }
-    return code;
+Expression * createArray(int len, Expression * e){
+    Expression * res = (Expression*) malloc(sizeof(Expression));
+    Type* t = (Type*) malloc(sizeof(Type));
+    t->name = ARRAY;
+    t->next = e->type;
+    res->type->name = t;
+    return res;
 }
-EXIT_CODE changeCharacterExpression(Expression* e,char value){
-    EXIT_CODE code;
-    if(e->type == UNKNOWN){
-        e->type = CHARACTER;
-    }
 
-    if(e->type != CHARACTER){
-        code = TYPE_ERROR;
-    } else {
-        e->value.char_value = value;
-        code = SUCCESS;
+void setIntegerValue(Expression * e,int value){
+    if(e->type == INTEGER){
+        e->val.int_value = value;
     }
-    return code;
 }
-EXIT_CODE changeArrayCharacterExpression(Expression* e,char value, int position){
-    EXIT_CODE code;
-    if(e->type == UNKNOWN){
-        e->type = ARRAY_CHARACTER;
-        e->len = position+1;
-        e->value.array_char_value = (char *) malloc(sizeof(char) * e->len);
-        int i;
-        for(i = 0; i < position; i++){
-            e->value.array_char_value[i] = '\0';
-        }
+void setDoubleValue(Expression * e, double value){
+    if(e->type == INTEGER){
+        e->val.double_value = value;
     }
-
-    if(e->type != ARRAY_CHARACTER){
-        code = TYPE_ERROR;
-    } else if(0 > position || position >= e->len) {
-        code = OUT_OF_BOUNDS_ERROR;
-    }else{
-        e->value.array_char_value[position] = value;
-        code = SUCCESS;
-    }
-    return code;
 }
-EXIT_CODE changeDoubleExpression(Expression* e,double value){
-    EXIT_CODE code;
-    if(e->type == UNKNOWN){
-        e->type = DOUBLE;
+void setBooleanValue(Expression * e, Boolean value){
+    if(e->type == INTEGER){
+        e->val.boolean_value = value;
     }
-
-    if(e->type != DOUBLE){
-        code = TYPE_ERROR;
-    } else {
-        e->value.double_value = value;
-        code = SUCCESS;
-    }
-    return code;
 }
-EXIT_CODE changeArrayDoubleExpression(Expression* e,double value, int position){
-    EXIT_CODE code;
-    if(e->type == UNKNOWN){
-        e->type = ARRAY_DOUBLE;
-        e->len = position+1;
-        e->value.array_double_value = (double *) malloc(sizeof(double) * e->len);
-        int i;
-        for(i = 0; i < position; i++){
-            e->value.array_double_value[i] = 0.0;
-        }
+void setCharacterValue(Expression * e, char value){
+    if(e->type == INTEGER){
+        e->val.char_value = value;
     }
-
-    if(e->type != ARRAY_DOUBLE){
-        code = TYPE_ERROR;
-    } else if(0 > position || position >= e->len) {
-        code = OUT_OF_BOUNDS_ERROR;
-    }else{
-        e->value.array_double_value[position] = value;
-        code = SUCCESS;
-    }
-    return code;
 }
-EXIT_CODE changeBooleanExpression(Expression* e,Boolean value){
-    EXIT_CODE code;
-    if(e->type == UNKNOWN){
-        e->type = BOOLEAN;
+Expression * get(int pos, Expression * e){
+    if(e->type == ARRAY){
+        e->value
     }
-
-    if(e->type != BOOLEAN){
-        code = TYPE_ERROR;
-    } else {
-        e->value.boolean_value = value;
-        code = SUCCESS;
-    }
-    return code;
-}
-EXIT_CODE changeArrayBooleanExpression(Expression* e,Boolean value, int position){
-    EXIT_CODE code;
-    if(e->type == UNKNOWN){
-        e->type = ARRAY_BOOLEAN;
-        e->len = position+1;
-        e->value.array_boolean_value = (Boolean *) malloc(sizeof(Boolean) * e->len);
-        int i;
-        for(i = 0; i < position; i++){
-            e->value.array_boolean_value[i] = FALSE;
-        }
-    }
-
-    if(e->type != ARRAY_BOOLEAN){
-        code = TYPE_ERROR;
-    } else if(0 > position || position >= e->len) {
-        code = OUT_OF_BOUNDS_ERROR;
-    }else{
-        e->value.array_boolean_value[position] = value;
-        code = SUCCESS;
-    }
-    return code;
 }
--- expression.h
+++ expression.h
@@ -6,54 +6,47 @@
 #include "error.h"
 
 typedef enum {
+    FALSE, TRUE
+} Boolean;
+
+typedef enum {
     INTEGER,
     CHARACTER,
     DOUBLE,
-    BOOLEAN, ARRAY,
+    BOOLEAN,
+    ARRAY,
     UNKNOWN
-} Type;
+} Typename;
+
+typedef struct S_Type {
+    Typename name;
+    struct S_Type* next;
+}Type;
 
 typedef struct{
     int size;
-    Type type;
     Value * value;
 }Array
-
-typedef enum {
-    FALSE, TRUE
-} Boolean;
 
 typedef union {
     int int_value;
     char char_value;
     double double_value;
     Boolean boolean_value;
-    Array array_value;
+    Array* array_value;
 } Value;
 
 typedef struct {
-    Type type;
-    Value value;
+    Type* type;
+    Value val;
 } Expression;
 
 Expression * createExpression();
 Expression * createIntegerExpression(int value);
-Expression * createArrayIntegerExpression(int * values, int len);
 Expression * createCharacterExpression(char value);
-Expression * createArrayCharacterExpression(char * values, int len);
 Expression * createDoubleExpression(double value);
-Expression * createArrayDoubleExpression(double * values, int len);
 Expression * createBooleanExpression(Boolean value);
-Expression * createArrayBooleanExpression(Boolean * values, int len);
-
-EXIT_CODE changeIntegerExpression(Expression* e,int value);
-EXIT_CODE changeArrayIntegerExpression(Expression* e,int value, int position);
-EXIT_CODE changeCharacterExpression(Expression* e,char value);
-EXIT_CODE changeArrayCharacterExpression(Expression* e,char value, int position);
-EXIT_CODE changeDoubleExpression(Expression* e,double value);
-EXIT_CODE changeArrayDoubleExpression(Expression* e,double value, int position);
-EXIT_CODE changeBooleanExpression(Expression* e,Boolean value);
-EXIT_CODE changeArrayBooleanExpression(Expression* e,Boolean value, int position);
+Expression * arrayConstructor(Expression* e, int len);
 
 
 #endif // EXPRESSION_H_INCLUDED
